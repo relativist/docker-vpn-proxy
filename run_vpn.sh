52ecd94438fcb2e0e6832ac3c06ed5c96631ec9c
Sitnikov <a.p.sitnikov@gmail.com>
2019-03-01 11:07:03 +0700

    fix

--- run_vpn.sh
+++ run_vpn.sh
@@ -3,7 +3,7 @@ d=$(date)
 echo "started at $d"
 echo "Now we run the file: "${VPN_FILE_NAME}
 
-/usr/sbin/openvpn --config /vpn/${VPN_FILE_NAME} --auth-user-pass creds.txt --management localhost 9999 &
+/usr/sbin/openvpn --config ${VPN_FILE_NAME} --auth-user-pass creds.txt --management localhost 9999 &
 sleep 10
 /usr/sbin/squid3 -f /etc/squid/squid.conf -N
 
