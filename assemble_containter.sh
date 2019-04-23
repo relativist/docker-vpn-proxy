3f118c5b2d26e338faa99456b44a8d738db7a4c2
Artem Sitnikov <a.p.sitnikov@gmail.com>
2019-04-23 19:42:07 +0700

    fix

--- assemble_containter.sh
+++ assemble_containter.sh
@@ -4,28 +4,31 @@ mkdir -p certificates
 cd certificates
 
 #1. fetch fresh certificates from www.vpnbook.com to folder "certificates/"
-wget https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-FR1.zip
-unzip -u VPNBook.com-OpenVPN-FR1.zip
-rm VPNBook.com-OpenVPN-FR1.zip*
-ls -la
+#wget https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-FR1.zip
+#unzip -u VPNBook.com-OpenVPN-FR1.zip
+#rm VPNBook.com-OpenVPN-FR1.zip*
+#ls -la
 
 #2. fetch fresh login and password for vpn
 echo "vpnbook" > creds.txt
-wget -O 1.png https://www.vpnbook.com/password.php
+#wget -O 1.png https://www.vpnbook.com/password.php
 
 
-answer=$(yad --center --title="PASSWORD" \
---image-on-top \
---text-align=left \
---text="Input below:" \
---image="1.png" \
---form \
---field="")
+#answer=$(yad --center --title="PASSWORD" \
+#--image-on-top \
+#--text-align=left \
+#--text="Input below:" \
+#--image="1.png" \
+#--form \
+#--field="")
 
-answer=$(echo $answer | tr -d '|')
-echo $answer
+#answer=$(echo $answer | tr -d '|')
+#echo $answer
 
-echo $answer >> creds.txt
+#echo $answer >> creds.txt
+password_twitter=$(wget -q -O - https://twitter.com/vpnbook | xmllint --html --xpath '//*[@id="stream-item-tweet-1120412018769575943"]/div[1]/div[2]/div[2]/p/text()' - 2>/dev/null | awk -F: '{print $3}' | tr -d ' ')
+#echo "zthc27c" >> creds.txt
+echo $password_twitter >> creds.txt
 
 #3. assemble docker image with name vpnka
 cd ../
--- run_vpn.sh
+++ run_vpn.sh
@@ -3,7 +3,32 @@ d=$(date)
 echo "started at $d"
 echo "Now we run the file: "${VPN_FILE_NAME}
 
+old_ip=$(curl ifconfig.me)
+echo "-------------------------------------"
+echo "OLD IP : "$old_ip
+echo "-------------------------------------"
 /usr/sbin/openvpn --config ${VPN_FILE_NAME} --auth-user-pass creds.txt --management localhost 9999 &
-sleep 15
-/usr/sbin/squid3 -f /etc/squid/squid.conf -N
+
+
+for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+sleep 1
+echo $var
+done
+
+
+new_ip=$(curl ifconfig.me)
+echo "-------------------------------------"
+echo "NEW IP : "$new_ip
+echo "-------------------------------------"
+if [ $old_ip == $new_ip ]
+then
+	echo "FAIL!"
+else
+	echo "SUCCESS!"
+fi
+echo "-------------------------------------"
+
+/usr/sbin/squid3 -f /etc/squid/squid.conf -N 
+#sleep 100000
 
